bb64fb8c61b1442b91c1f7eba2a6c0514a48d755
SelinaKudadze11105718 <seakudadze001@st.ug.edu.gh>
2024-07-05 22:23:42 +0000

    Methods to read longitudes,latitudes and landmarks from the csv file

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
         ArrayList<String[]> csvContents = ReadCSV.readCSV("UG-locations.csv");
         ReadCSV.makeContentsReadable(csvContents);
+        System.out.println();
+        System.out.println(ReadCSV.getLongitude(csvContents,"Commonwealth Hall"));
+//        System.out.println();
+        System.out.println(ReadCSV.getLatitude(csvContents,"Commonwealth Hall"));
+        System.out.println(ReadCSV.getLandmark(csvContents,"Commonwealth Hall"));
+//        System.out.println();
+        System.out.println(ReadCSV.getLongitude(csvContents,"Commonwealth"));
+//        System.out.println();
+        System.out.println(ReadCSV.getLatitude(csvContents,"Commonwealth"));
+        System.out.println(ReadCSV.getLandmark(csvContents,"Commonwealth"));
     }
 }
--- ReadCSV.java
+++ ReadCSV.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ReadCSV {
     public static ArrayList<String[]> readCSV(String file) {
@@ -27,20 +28,44 @@ public class ReadCSV {
         }
         return csvContents;
     }
-    public static void makeContentsReadable(ArrayList<String[]> csvContents){
+
+    public static void makeContentsReadable(ArrayList<String[]> csvContents) {
         for (String[] row : csvContents) {
             for (String field : row) {
                 System.out.print(field + " ");
+
             }
             System.out.println();
         }
     }
-    public static void getLongitude(ArrayList<String[]> csvContents){
+
+    public static Double getLongitude(ArrayList<String[]> csvContents, String location) {
         for (String[] row : csvContents) {
-            for (String field : row) {
-                System.out.print(field + " ");
-            }
-            System.out.println();
+                if (row[0].toUpperCase().equals(location.toUpperCase())) {
+                    return Double.parseDouble(row[1]);
+                }
         }
+        return Double.parseDouble("0");
     }
+
+    public static Double getLatitude(ArrayList<String[]> csvContents, String location) {
+        for (String[] row : csvContents) {
+                if (row[0].toUpperCase().equals(location.toUpperCase())) {
+                    return Double.parseDouble(row[2]);
+                }
+        }
+        return Double.parseDouble("0");
+    }
+
+    public static String getLandmark(ArrayList<String[]> csvContents, String location){
+        for (String[] row : csvContents) {
+                if (row[0].toUpperCase().equals(location.toUpperCase())) {
+                    return row[3];
+                }
+        }
+        return "No landmark allocated";
+
+    }
+
 }
+

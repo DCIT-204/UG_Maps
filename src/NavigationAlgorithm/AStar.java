5297cf3f4d9fd0ce531a3202ef2605362477414f
gene-999 <eugenebaidoo5@gmail.com>
2024-07-27 15:24:40 +0000

    feat: custom implementation of A* search that gets all possible routes and distances

--- src/NavigationAlgorithm/AStar.java
+++ src/NavigationAlgorithm/AStar.java
@@ -3,69 +3,68 @@ package src.NavigationAlgorithm;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.Set;
 
 
 public class AStar {
-
     static ArrayList<List<Integer>> routes = new ArrayList<>();
+    static ArrayList<Double> distances = new ArrayList<>();
     static double dist = 0;
-    static ArrayList<Double> molly = new ArrayList<>();
-
-    static List<List<Integer>> getSolution(Node node) {
+    static void getSolution(Node node, int end){
         ArrayList<Integer> path = new ArrayList<>();
-        double totalDistance = 0;
-
-        while (node != null) {
-            path.add(0, node.id); // Add elements to the front for proper ordering
-            if (node.parent != null) {
-                totalDistance += Util.getDistance(node.parent.id, node.id);
+        while(node.parent != null){
+            path.add(node.id);
+            if(node.id == end) {
+                dist = node.totalDistance;
             }
             node = node.parent;
         }
-
-        molly.add(totalDistance);
-        System.out.println(path);
-        System.out.println("Distance is " + totalDistance);
-        routes.add(path);
-        System.out.println(molly);
-        return routes;
+        path.add(node.id);
+        distances.add(dist);
+        routes.add(path.reversed());
     }
-
     public static int Astar(int start, int end) {
         Set<Integer> visited = new HashSet<>();
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        queue.add(new Node(start, 0, null));
+        CustomPriorityQueue queue = new CustomPriorityQueue();
+        queue.add(new Node(start, null, 0, 0));
 
         while (!queue.isEmpty()) {
-            Node node = queue.poll();
-
-            if (node.id == end) {
-                getSolution(node);
-                return 0; // Path found
+            Node node = queue.remove();
+            if (node == null) {
+                break;
             }
 
-            if (visited.contains(node.id)) {
-                continue;
-            }
+            if (node.id != Integer.MAX_VALUE) {
+                int currentNode = node.id;
+                double currentCost = node.costToNode;
 
-            visited.add(node.id);
+                if (currentNode == end) {
+                    getSolution(node,end);
+                    continue;
+                }
 
-            for (int neighbour : Util.getNeighbours(node.id)) {
-                if (!visited.contains(neighbour)) {
-                    double newDistance = node.distance + Util.getDistance(node.id, neighbour);
-                    double priority = newDistance + Util.getDistance(neighbour, end);
-                    queue.add(new Node(neighbour, priority, node));
+                if (visited.contains(currentNode)) {
+                    continue;
+                }
+
+                visited.add(currentNode);
+
+                for (int neighbour: Util.getNeighbours(currentNode)) {
+                    double newDistance = currentCost + Util.getDistance(currentNode, neighbour);
+                    double heuristic = Util.getDistance(neighbour, end);
+
+                    queue.add(new Node(neighbour, node, newDistance, heuristic));
                 }
             }
         }
 
         System.out.println(routes);
-        return -1; // Return -1 if there is no path found
+        System.out.println(distances);
+        return -1;
     }
-
-    public static void main(String[] args) {
-        System.out.println(Astar(1, 24));
+    public static void main(String[] args){
+        Astar(113,24);
     }
 }
+
+
